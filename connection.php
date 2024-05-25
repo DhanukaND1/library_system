bd173e1cd4fa77a52908f0a8fde17649f66ddf73
DhanukaND1 <2022t01555@stu.cmb.ac.lk>
2024-05-25 13:05:50 +0530

    created a database connection

---
+++ connection.php
@@ -0,0 +1,20 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "library_system";
+
+//create connection
+
+$conn = new mysqli($servername,$username,$password,$dbname,3307);
+
+//check connection
+
+if($conn->connect_error){
+  die("Connection Failed: ".$conn->connect_error);
+  
+}
+//echo "Connection Successful";
+
+?>
