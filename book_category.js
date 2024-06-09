7d542c306adbdcc90e1415e3f1d4b2e1940b58c6
Geethanjana karunarathna <2022t01541@stu.cmb.ac.lk>
2024-06-09 12:49:24 +0530

    Update book_category.js
    Show edit form and Show delete confirmation form

--- book_category.js
+++ book_category.js
@@ -16,6 +16,31 @@ window.onclick = function(event) {
         regModal.style.display = "none";
     }
 }
+// Show edit modal
+document.querySelectorAll('.btn-success').forEach(function(editBtn) {
+    editBtn.onclick = function(event) {
+        var categoryEditModel = event.target.closest("td").querySelector(".edit");
+        categoryEditModel.style.display = "block";
+
+        var editSpan = categoryEditModel.getElementsByClassName("eclose")[0];
+        editSpan.onclick = function() {
+            categoryEditModel.style.display = "none";
+        }
+
+        window.onclick = function(event) {
+            if (event.target == categoryEditModel) {
+                categoryEditModel.style.display = "none";
+            }
+        }
+    }
+});
+
+document.querySelectorAll('.eclose').forEach(function(element) {
+    element.addEventListener('click', function() {
+        // Hide the alert
+        this.parentElement.style.display = 'none';
+    });
+});
 
 // Form validation
 var regForm = document.getElementById('frm');
@@ -38,5 +63,31 @@ regForm.onsubmit = function() {
 
 editForm.onsubmit = function() {
     return validateForm(editForm);
-}
-;
+};
+// Show delete confirmation form
+document.querySelectorAll('.delete-btn').forEach(function(deleteBtn) {
+    deleteBtn.onclick = function(event) {
+        var categoryId = event.target.getAttribute('data-id');
+        var confirmForm = document.getElementById('deleteConfirm');
+        var deleteMessage = confirmForm.querySelector('#deleteMessage');
+        deleteMessage.textContent = 'Do you want to delete ' + categoryId + ' category?';
+        confirmForm.querySelector('.confirm-delete').setAttribute('data-id', categoryId);
+        confirmForm.style.display = 'block';
+        document.body.classList.add('modal-open');
+    }
+});
+
+document.querySelectorAll('.confirm-delete').forEach(function(confirmDeleteBtn) {
+    confirmDeleteBtn.onclick = function(event) {
+        var categoryId = event.target.getAttribute('data-id');
+        window.location.href = 'book_category_process.php?delete=' + categoryId;
+    }
+});
+
+document.querySelectorAll('.cancel-delete').forEach(function(cancelDeleteBtn) {
+    cancelDeleteBtn.onclick = function() {
+        var confirmForm = document.getElementById('deleteConfirm');
+        confirmForm.style.display = 'none';
+        document.body.classList.remove('modal-open');
+    }
+});
